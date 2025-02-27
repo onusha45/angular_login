94b36291eebc941888628bcdaf14f88f75e183cc
hatsimitsu <onusha45@gmail.com>
2025-02-27 17:09:00 +0545

    CRUD completion

--- loginangular/src/app/Components/put-postapi/put-postapi.component.ts
+++ loginangular/src/app/Components/put-postapi/put-postapi.component.ts
@@ -1,29 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { deptuserObj } from '../../../model/department';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-put-postapi',
-  imports: [FormsModule],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './put-postapi.component.html',
   styleUrl: './put-postapi.component.css'
 })
 export class PutPostapiComponent implements OnInit{
-departmentList : any[]=[];
-deptuserObj:any ={
-  "departmentId":0,
-  "departmentName":"",
-  "departmentLogo":""
-}
+departmentList : deptuserObj[]=[];
+deptuserObj:deptuserObj=new deptuserObj();
+isdelete:boolean = true;
 http= inject(HttpClient);
+
 ngOnInit(): void {
   this.getDepartment();
+  debugger
     
 }
 onSave(){
+  console.log(JSON.stringify(this.deptuserObj))
   debugger;
   this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment",this.deptuserObj).subscribe((res:any)=>{
-    debugger;
     if(res.result){
       alert("Department Created sucessfulley")
       this.getDepartment();
@@ -43,5 +45,31 @@ onEdit(data: any){
   this.deptuserObj = data;
 
 }
+onUpdate(){
+  console.log(JSON.stringify(this.deptuserObj))
+  debugger;
+  this.http.post("https://projectapi.gerasim.in/api/Complaint/UpdateDepartment",this.deptuserObj).subscribe((res:any)=>{
+    if(res.result){
+      alert("Department Created sucessfulley")
+      this.getDepartment();
+    }else{
+      alert(res.message)
+    }
+  })
+  
+}
+onDelete(id:number){
+  alert("Are you sure you want to delete?")
+  debugger;
+  if (this.isdelete){
+  this.http.delete("https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId?departmentId=" +id).subscribe((res:any)=>{
+    if(res.result){
+      alert("Department deleted sucessfulley")
+      this.getDepartment();
+    }else{
+      alert(res.message)
+    }
+  })}
 
+}
 }
---
+++ loginangular/src/model/department.ts
@@ -0,0 +1,14 @@
+export class deptuserObj{
+    departmentId: number ;
+    departmentName:string;
+    departmentLogo:string ;
+
+    constructor(){
+        this.departmentId =0;
+        this.departmentName="";
+        this.departmentLogo="";
+        
+    }
+
+
+  }
